763615fd77b7a0b240ebb365925c485e27d005fd
alunosNET <alunosnet@gmail.com>
2023-03-16 14:08:48 +0000

    V2 do projeto fórum - continuação

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect, session
 from basedados import *
 import utilizador
 import tema
 import mensagem
 import os
+from flask_session import Session
 
 #Criar a bd e as tabelas
-tabela_utilizador="create table if not exists utilizador(id integer primary key autoincrement, nome text, email text, palavra_passe text, estado integer)"
+tabela_utilizador="create table if not exists utilizador(id integer primary key autoincrement, nome text, email text, palavra_passe text, estado integer, perfil integer)"  #0-admin 1-user
 tabela_mensagem="create table if not exists mensagem(id integer primary key autoincrement, texto text, data_hora_mensagem numeric, id_utilizador integer references utilizador(id), id_mensagem_original integer references mensagem(id))"
 tabela_tema="create table if not exists tema(id integer primary key autoincrement, nome text, estado integer)"
 
@@ -17,6 +18,10 @@ ExecutarSQL(tabela_tema)
 #Criar a aplicação Flask
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
 #definir a pasta para guardar ficheiros
 UPLOAD_FOLDER=os.path.join(app.root_path,"static/imagens")
 app.config["UPLOAD_FOLDER"]=UPLOAD_FOLDER
@@ -26,78 +31,155 @@ app.config["UPLOAD_FOLDER"]=UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
+@app.route('/login',methods=["POST","GET"])
+def login():
+    return utilizador.login()
+
+@app.route('/logout')
+def logout():
+    session["id"]=None
+    session["nome"]=None
+    session["email"]=None
+    session["perfil"]=None
+    return redirect("/")
+
 #rotas da mensagem
 @app.route("/mensagem/apagar",methods=["POST"])
 def mensagem_apagar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return mensagem.mensagem_apagar()
 
 @app.route("/mensagem/apagar_confirmado",methods=["POST"])
 def mensagem_apagar_confirmado():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return mensagem.mensagem_apagar_confirmado()
-
 
 @app.route("/mensagem/guardar_resposta",methods=["POST"])
 def mensagem_guardar_resposta():
+    if "perfil" not in session:
+        return redirect("/login")
     return mensagem.mensagem_guardar_resposta()
     
 @app.route("/mensagem/responder",methods=["POST"])
 def mensagem_responder():
+    if "perfil" not in session:
+        return redirect("/login")
     return mensagem.mensagem_responder()
 
 @app.route('/forum',methods=["GET","POST"])
 def forum_index():
+    if "perfil" not in session:
+        return redirect("/login")
     return mensagem.mensagem_listar()
 
 #rotas do tema
 @app.route('/tema/adicionar',methods=["GET","POST"])
 def tema_adicionar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_adicionar()
     
 @app.route('/tema/listar')
 def tema_listar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_listar()
 
 @app.route('/tema/apagar',methods=["POST"])
 def tema_apagar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_apagar()
 
 @app.route('/tema/apagar_confirmado',methods=["POST"])
 def tema_apagar_confirmado():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_apagar_confirmado()
 
 @app.route('/tema/editar',methods=["POST"])
 def tema_editar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_editar()
 
 @app.route('/tema/editar_confirmado',methods=["POST"])
 def tema_editar_confirmado():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return tema.tema_editar_confirmado(app)
 
 #Rotas do utilizador
+@app.route('/utilizador/criar',methods=["GET","POST"])
+def utilizador_criar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
+    return utilizador.utilizador_criar(app)
+
 @app.route('/utilizador/registar',methods=["GET","POST"])
 def utilizador_registar():
     return utilizador.utilizador_registar(app)
 
 @app.route('/utilizador/listar')
 def utilizador_listar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return utilizador.utilizador_listar()
 
 @app.route('/utilizador/apagar',methods=["POST"])
 def utilizador_apagar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return utilizador.utilizador_apagar()
 
 @app.route("/utilizador/apagar_confirmado",methods=["POST"])
 def utilizador_apagar_confirmado():
-    return utilizador.utilizador_apagar_confirmado()
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
+    return utilizador.utilizador_apagar_confirmado(app)
 
 #rota para editar
 @app.route("/utilizador/editar",methods=["POST"])
 def utilizador_editar():
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
     return utilizador.utilizador_editar()
 
 #rota para atualizar dados
 @app.route("/utilizador/editar_confirmado",methods=["POST"])
 def utilizador_editar_confirmado():
-    return utilizador.utilizador_editar_confirmado()
+    if "perfil" not in session:
+        return redirect("/login")
+    if session["perfil"]!=0:
+        return redirect("/login")
+    return utilizador.utilizador_editar_confirmado(app)
 
 app.run()
--- utilizador.py
+++ utilizador.py
@@ -1,9 +1,61 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect, session
 from basedados import *
 import os
 from werkzeug.utils import secure_filename
+from flask_session import Session
 
-#TODO: guardar fotografia
+def login():
+    if request.method=="GET":
+        return render_template('utilizador/login.html')
+    if request.method=="POST":
+        #pesquisar na bd o utilizador
+        email = request.form.get("email")
+        palavra_passe=request.form.get("palavra_passe")
+        sql="SELECT * FROM utilizador WHERE email=? AND palavra_passe=?"
+        parametros=(email,palavra_passe)
+        dados = DevolverSQL(sql,parametros)
+        if len(dados)!=1:
+            mensagem="O login falhou. Tente novamente."
+            return render_template("utilizador/login.html",mensagem=mensagem)
+        #iniciar sessão
+        #guardar o id, email, nome, perfil
+        session["email"]=email
+        session["id"]=dados[0]['id']
+        session["nome"]=dados[0]['nome']
+        session["perfil"]=dados[0]['perfil']
+        return redirect("/")
+
+#só para admin
+def utilizador_criar(app):
+    if request.method=="GET":
+        return render_template('utilizador/criar.html')
+    if request.method=="POST":
+        #recolher os dados do form
+        nome = request.form.get("nome")
+        email = request.form.get("email")
+        palavra_passe = request.form.get("palavra_passe")
+        perfil = request.form.get("perfil")
+        #validar os dados
+        if not nome or not email or not palavra_passe:
+            return render_template('utilizador/criar.html',mensagem="Tem de preencher todos os campos")
+        
+        sql="SELECT * FROM Utilizador WHERE email=?"
+        parametros=(email,)
+        dados = DevolverSQL(sql,parametros)
+        if dados:
+            return render_template('utilizador/criar.html',mensagem="O email já existe")
+
+        ficheiro = request.files["fotografia"]
+        #nome_ficheiro=secure_filename(ficheiro.filename)
+        ficheiro.save(os.path.join(app.config["UPLOAD_FOLDER"],nome))
+
+        #adicionar a bd
+        sql="INSERT INTO Utilizador(nome, email, palavra_passe, estado, perfil) values(?,?,?,?,?)"
+        parametros=(nome,email,palavra_passe,1,perfil)
+        ExecutarSQL(sql,parametros)
+        #redirecionar
+        return redirect('/utilizador/listar')
+
 def utilizador_registar(app):
     if request.method=="GET":
         return render_template('utilizador/registar.html')
@@ -27,7 +79,7 @@ def utilizador_registar(app):
         ficheiro.save(os.path.join(app.config["UPLOAD_FOLDER"],nome))
 
         #adicionar a bd
-        sql="INSERT INTO Utilizador(nome, email, palavra_passe, estado) values(?,?,?,?)"
+        sql="INSERT INTO Utilizador(nome, email, palavra_passe, estado, perfil) values(?,?,?,?,1)"
         parametros=(nome,email,palavra_passe,1)
         ExecutarSQL(sql,parametros)
         #redirecionar
@@ -49,9 +101,14 @@ def utilizador_apagar():
     #mostrar a página para confirmar o apagar
     return render_template("utilizador/apagar.html",registo=dados[0])
 
-def utilizador_apagar_confirmado():
+def utilizador_apagar_confirmado(app):
     #recolher o id do utilizador a apagar
     id = request.form.get("id")
+    #apagar fotografia
+    nome = request.form.get("nome")
+    nome=os.path.join(app.config["UPLOAD_FOLDER"],nome)
+    if os.path.exists(nome):
+        os.remove(nome)
     #apagar da bd
     sql = "DELETE FROM Utilizador WHERE id=?"
     parametros=(id,)
@@ -77,8 +134,9 @@ def utilizador_editar_confirmado(app):
         return render_template("utilizador/editar.html",mensagem="Tem de preencher todos os campos")
     
     ficheiro = request.files["fotografia"]
-    #Francisco vai fazer um aviso para perguntar ao utilizador se quer substituir
-    ficheiro.save(os.path.join(app.config["UPLOAD_FOLDER"],nome))
+    if ficheiro:
+        #testar se escolheu enviar nova foto
+        ficheiro.save(os.path.join(app.config["UPLOAD_FOLDER"],nome))
     #atualizar a bd
     sql = "UPDATE Utilizador SET nome=?, email=?, palavra_passe=? WHERE id=?"
     parametros=(nome,email,password,id)
